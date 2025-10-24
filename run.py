e2da080eb08635eda1be1008f02ed1fbfa44f776
Shardul <118350178+shardulspy@users.noreply.github.com>
2025-10-24 16:16:26 +0530

    run.py

--- run.py
+++ run.py
@@ -1,1 +1,11 @@
+import piexif
+from PIL import Image
+
+def remove_exif(input_path, output_path):
+    img = Image.open(input_path)
+    img.save(output_path, exif=b'')
+    print(f"✅ {output_path} saved without EXIF data.")
+
+if __name__ == "__main__":
+    remove_exif("input.jpg", "output.jpg")
 
